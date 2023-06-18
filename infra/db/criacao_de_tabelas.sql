3187c7ad96757882ab2cc560b36da100a37a4796
Rob <robson.santana.soares@gmail.com>
2023-06-18 18:15:45 -0300

    Inclusão de frontend

--- infra/db/criacao_de_tabelas.sql
+++ infra/db/criacao_de_tabelas.sql
@@ -10,122 +10,61 @@ CREATE TABLE candidatos (
   PRIMARY KEY (can_id)
 );
 
-CREATE TABLE matricula (
-  can_id int NOT NULL,
-  cur_id int NOT NULL,
+CREATE TABLE matriculas (
+  mat_id int NOT NULL AUTO_INCREMENT,
+  mat_nome VARCHAR(200) COLLATE utf8_bin NOT NULL,
   
-  CONSTRAINT fk_can FOREIGN KEY (can_id) REFERENCES candidatos (can_id),
-  CONSTRAINT fk_cur FOREIGN KEY (cur_id) REFERENCES cursos (cur_id)
+  PRIMARY KEY (mat_id)
 );
 
 CREATE TABLE interesses (
-  cur_int_id int NOT NULL,
-  can_int_id int NOT NULL,
+  int_id int NOT NULL AUTO_INCREMENT,
+  int_nome VARCHAR(200) COLLATE utf8_bin NOT NULL,
+  int_prioridade int NOT NULL,
 
-  CONSTRAINT fk_can_int FOREIGN KEY (can_int_id) REFERENCES candidatos (can_id),
-  CONSTRAINT fk_cur_int FOREIGN KEY (cur_int_id) REFERENCES cursos (cur_id)
+  PRIMARY KEY (int_id)
 );
 
-CREATE TABLE cursos (
-  cur_id INT NOT NULL,
-  cur_nome VARCHAR(200) COLLATE utf8_bin NOT NULL,
-  cur_descricao VARCHAR(500) COLLATE utf8_bin NOT NULL
+CREATE TABLE candidatos_interesses (
+  cin_can_id int NOT NULL,
+  cin_int_id int NOT NULL,
+
+  CONSTRAINT pk_can_int PRIMARY KEY (cin_can_id, cin_int_id)
 );
 
-CREATE TABLE endereco (
+CREATE TABLE candidatos_matriculas (
+  cma_can_id int NOT NULL,
+  cma_mat_id int NOT NULL,
+
+  CONSTRAINT pk_can_mat PRIMARY KEY (cma_can_id, cma_mat_id)
+);
+
+CREATE TABLE enderecos (
   end_id int NOT NULL AUTO_INCREMENT,
   end_logradouro varchar(250) COLLATE utf8_bin NOT NULL,
   end_cep varchar(10) COLLATE utf8_bin NOT NULL,
   end_numero int NOT NULL,
+  end_cidade varchar(255),
+  end_estado varchar(255),
   PRIMARY KEY(end_id)
 );
 
-CREATE TABLE cidades (
-  cid_id int(100) NOT NULL,
-  cid_nome varchar(100) COLLATE utf8_bin NOT NULL,
-  cid_est_id int(100) NOT NULL
-);
-
-CREATE TABLE estados (
-  est_id int(100) NOT NULL,
-  est_nome varchar(100) COLLATE utf8_bin NOT NULL,
-  est_capital_id int(100) NOT NULL,
-  est_sigla varchar(10) COLLATE utf8_bin NOT NULL
-);
-
 CREATE TABLE telefones (
-  tel_id int NOT NULL,
-  tel_numero int NOT NULL,
-  tel_tipo varchar(60) COLLATE utf8_bin NOT NULL
+  tel_id int NOT NULL AUTO_INCREMENT,
+  tel_numero VARCHAR(60) NOT NULL,
+  tel_tipo varchar(60) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (tel_id)
 );
 
 CREATE TABLE candidatos_enderecos (
   cen_can_id int NOT NULL,
-  cen_end_id int NOT NULL
+  cen_end_id int NOT NULL,
+
+  CONSTRAINT pk_can_end PRIMARY KEY (cen_can_id, cen_end_id)
 );
 
 CREATE TABLE candidatos_telefones (
   ctl_tel_id int NOT NULL,
-  ctl_can_id int NOT NULL
+  ctl_can_id int NOT NULL,
+  CONSTRAINT pk_can_tel PRIMARY KEY (ctl_tel_id, ctl_can_id)
 );
-
-
-
-
-
-ALTER TABLE cidades
-  ADD PRIMARY KEY (cid_id),
-  ADD KEY fk_cid_est (cid_est_id);
-
-ALTER TABLE endereco
-  ADD PRIMARY KEY (end_id);
-
-ALTER TABLE cursos
-  ADD PRIMARY KEY (cur_id);
-
-ALTER TABLE cursos_interessados
-  ADD PRIMARY KEY (cin_id);
-
-ALTER TABLE estados
-  ADD PRIMARY KEY (est_id),
-  ADD KEY fk_est_cid_id (est_capital_id);
-
-ALTER TABLE telefones
-  ADD PRIMARY KEY (tel_id);
-
-ALTER TABLE cursos_interessados
-  MODIFY cur_id int NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE cursos
-  MODIFY cin_id int NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE endereco
-  MODIFY end_id int NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE telefones
-  MODIFY tel_id int NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE cidades
-  ADD CONSTRAINT fk_cid_est FOREIGN KEY (cid_est_id) REFERENCES estados (est_id);
-
-ALTER TABLE estados
-  ADD CONSTRAINT fk_est_cid FOREIGN KEY (est_capital_id) REFERENCES cidades (cid_id);
-
-ALTER TABLE candidatos_enderecos
-  ADD CONSTRAINT pk_can_end PRIMARY KEY (cen_can_id, cen_end_id);
-
-ALTER TABLE candidatos_enderecos
-  ADD CONSTRAINT fk_cen_can FOREIGN KEY (cen_can_id) REFERENCES candidatos (can_id);
-
-ALTER TABLE candidatos_enderecos
-  ADD CONSTRAINT fk_cen_end FOREIGN KEY (cen_end_id) REFERENCES enderecos (end_id);
-
-ALTER TABLE candidatos_telefones
-  ADD CONSTRAINT pk_can_tel PRIMARY KEY (ctl_can_id, ctl_tel_id);
-
-ALTER TABLE candidatos_telefones
-  ADD CONSTRAINT fk_ctl_tel FOREIGN KEY (ctl_tel_id) REFERENCES telefones (tel_id);
-  
-ALTER TABLE candidatos_telefones
-  ADD CONSTRAINT fk_ctl_can FOREIGN KEY (ctl_can_id) REFERENCES candidatos (can_id);
-
